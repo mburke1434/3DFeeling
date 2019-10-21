a952cbe3616e041d8882b77e654a794033612430
Jonathan Buchanan <johnyburd@gatech.edu>
2019-10-21 13:02:11 -0400

    routing

--- website/src/App.js
+++ website/src/App.js
@@ -2,20 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Link } from 'react-router-dom'
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {FormControl, Form, Button} from 'react-bootstrap';
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Enter some text to get a nice 3d model :)
-        </p>
-	  <form>
-	    <label>
-	      Text:
-	      <input type="text" name="Text" />
-	    </label>
-	    <input type="submit" value="Submit" />
-	  </form>
+
+        <Form>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Please enter a text sample!</Form.Label>
+            <Form.Control as="textarea" size="lg" rows="6" />
+            <Link to="/users">
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Link>
+          </Form.Group>
+        </Form>
       </header>
     </div>
   );
