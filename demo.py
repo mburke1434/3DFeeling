673a7906e0d4a13ef6888fdaf64aed79c2d62183
aschott6 <aschott6@gatech.edu>
2019-11-18 09:07:03 -0500

    Finished demo.

--- demo.py
+++ demo.py
@@ -2,6 +2,8 @@ import nlp.SentimentClassifier as SentimentClassifier
 import shape
 import nltk
 from nltk import sent_tokenize
+import os
+
 
 vad_classifier = SentimentClassifier.VADClassifier()
 while True:
@@ -12,6 +14,7 @@ while True:
         print(sentence)
         print('valence, arousal and dominance: ', vad)
         points.append((vad[0], vad[1]))
-        test_shape = shape.Representation(points)
-        test_shape.get_final_shape().write("demo.scad")
+    test_shape = shape.Representation(points)
+    test_shape.get_final_shape().write("demo.scad")
+    os.system("start " + "demo.scad")
 
